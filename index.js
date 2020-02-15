de4395550a59ad302c6bcbfc042f80aae2bafd84
Harish HB <harish.8990@gmail.com>
2020-02-15 14:58:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require("http");
 const hostname = '127.0.0.1';
 const sfdx = require('sfdx-node');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const express = require('express')
 const app = express();
